b78b62a4f84d0304f33915f15c55504446f39a7d
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-01-29 19:14:11 +0530

    add variables folder

---
+++ Variables/data_types.py
@@ -0,0 +1,14 @@
+# Python : Supports several data_types __builtins__
+# Integers : Whole numbers (eg: 1, -5, 42)
+# Floats : Decimal numbers (eg: 3.14, -0.001, 2.0)
+# Strings : Text (eg: "Hello", 'World')
+# Booleans : True/False values
+# Lists: ordered, mutable collections (eg : [1,2,3], [a,b,c])
+# Tuples: orderd , immutable collections (eg: (1,2,3), (a,b,c))
+# set : unorder collection of unique numbers (eg: {1,2,3}, {a,b,c})
+# Dictionories : keys nad values pairs (eg : {"name : "Alice", "age": 30})
+                                            
+
+a = 34
+print(a)
+print(type(a))
