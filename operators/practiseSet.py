2cbd26eb8f2bf4acbee0fc747d2be41c3bf65076
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-02-10 01:37:36 +0530

    add some code

--- operators/practiseSet.py
+++ operators/practiseSet.py
@@ -3,3 +3,11 @@ print("Hello , world! welcome to python")
 
 # print a poem
 print("Twinkle twinkle little star, \n how are a wonder what you are")
+
+name = "Guddiya"
+age = 25
+height = 5.5
+is_student = True
+
+print("The name of the student is" + name + "and her age is" + str(age) + "years. "
+"she is " + str(height) + "feet tall and it is " + str(is_student) + "that she is a student.")
