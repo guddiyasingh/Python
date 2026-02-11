aed75902b159e3f392cfcdf963484ad689409ed9
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-02-11 07:43:25 +0530

    add some qustions and answer in practiseSet file

--- operators/practiseSet.py
+++ operators/practiseSet.py
@@ -15,3 +15,28 @@ print("The name of the student is" + name + "and her age is" + str(age) + "years
 # convert it into an integer and add 10 to it print the result
 num ="20"
 print (int(num) + 10)
+
+
+food  = input("what is your favourite food? \n")
+
+print("my favourite food is " +food)
+
+
+# Q 6 Simple calculator
+
+# write a program 
+# 1. takes two numbers as input from users
+
+a = int(input("Enter the first number:"))
+b =int(input("Enter the second number:"))
+
+print("The sum of the two numbers is " + str(a+b))
+print("The difference of the two numbers is " + str(a-b))
+print("The product of the two numbers is " + str(a*b))
+print("The quotient of the two numbers is " + str(a/b))
+
+
+# Escape sequences 
+# print the following output using escape sequences
+
+print("Harry said python is awsome, \n This is anew line.'\t' this is a tab space")
