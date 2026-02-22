cf10574ad3b924762e5397822efc5411e36fef9e
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-02-22 14:41:08 +0530

    add two files problem3.1.py, problem.3.3.py

---
+++ users_input/problem3.1.py
@@ -0,0 +1,4 @@
+# print a number 1 to 10 to use a for loop
+
+for i in range(1, 11):
+    print(i)
---
+++ users_input/problem3.2.py
@@ -0,0 +1,8 @@
+# Print the multiplication table of number(enter by user)
+
+num = int(input("Enter any number: "))
+
+
+for i in range(1, 11):
+
+ print(num*i)
