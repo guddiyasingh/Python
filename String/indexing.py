86f932fa53b1750fffaa4653ea1ebde9345055f4
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-02-26 22:49:03 +0530

    add some code

---
+++ String/indexing.py
@@ -0,0 +1,14 @@
+name = "Guddiya"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+print("reverse number \n")
+
+print(name[-5])
+print(name[-4])
+print(name[-3])
+print(name[-2])
+print(name[-1])
+print(name[0])
