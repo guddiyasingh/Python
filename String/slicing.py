7cbdecb4a4d4939d0ae091acedb455fb050a9ebe
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-02-27 15:15:09 +0530

    add slicing.py file

---
+++ String/slicing.py
@@ -0,0 +1,12 @@
+name = "Guddiya"
+
+print(name[0:2])  # goes from 0 to 2-1 means 0-1
+print(name[2:4]) # goes from 2-3
+print(name[4:6]) # goes from 4-5
+print(name[6:7])
+
+name1 = "13345xysde"
+print("Enter index number \n")
+print(name1[0:10:4])
+print(name1[0:10:3])
+print(name1[0:10:1])
