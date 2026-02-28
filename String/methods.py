b78ce277426c291514f5584f39bebeeb4d46be8f
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-01 01:23:38 +0530

    add some code in method file

--- String/methods.py
+++ String/methods.py
@@ -9,19 +9,28 @@ name = "Guddiya singh" # strings are immutable
 # print(name.capitalize())
 # print(name.title())
 
-text = " hello world "
-print(text)
-print(text.strip())  # Output "hello world"
-print(text.lstrip()) # Output " hello world"
-print(text.rstrip()) # output "hello world "
+# text = " hello world "
+# print(text)
+# print(text.strip())  # Output "hello world"
+# print(text.lstrip()) # Output " hello world"
+# print(text.rstrip()) # output "hello world "
 
 
-text = "python is fun and fun"
+# text = "python is fun and fun"
 
-print(text.find("is"))
-print(text.replace("fun","awesome"))
+# print(text.find("is"))
+# print(text.replace("fun","awesome"))
 
-fruits = "Apple, bnana, Lichi"
+# fruits = "Apple, bnana, Lichi"
 
-print(fruits.split())
+# print(fruits.split(","))
+# print(",".join(["Apple, bnana, Lichi"]))
+
+
+text = "Guddiyakumari345"
+
+print(text.isalpha()) # Output false
+print(text.isdigit()) # Output false
+print(text.isalnum()) # output true
+print(text.isspace()) # Output false
 
