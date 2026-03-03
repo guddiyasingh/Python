c965bb2e7a3718c8a99868e25d2fdba844609161
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-03 23:51:15 +0530

    add some code

--- String/formating.py
+++ String/formating.py
@@ -1,3 +1,13 @@
 # string formating
 
 template = "Dear {} , you are awesome. Take this {} $ beg"
+
+a = "johan"
+a1 = 10000
+b = "jack"
+b1 = 10000
+c2 = 300
+
+s1 = template.format(a,a1)
+
+print(s1)
