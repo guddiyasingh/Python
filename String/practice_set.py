5d8166b4c7652594156fac59bbe72cece15c6924
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-05 01:50:43 +0530

    add practice_set file

---
+++ String/practice_set.py
@@ -0,0 +1,17 @@
+# Create a string variable "name" with you full name .Print
+
+# 1. 1st chracter
+# 2. 2nd chracter
+# 3. The length of string
+
+name = "Guddiya"
+print(name[0]) # print 1st character
+print(name[-1]) # print 2nd character
+print(len(name))
+
+# b. concatinate two strings "Hello" and "World" with a space between
+
+a = "Hello"
+b = "World"
+
+print(a + " " + b)
