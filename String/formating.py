5604102c2c9b753590e84512d8af43cb0b3df01b
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-04 23:58:32 +0530

    add some code in formating file

--- String/formating.py
+++ String/formating.py
@@ -11,3 +11,11 @@ c2 = 300
 s1 = template.format(a,a1)
 
 print(s1)
+
+
+# Write your code below to print the required output using escape sequences
+a = "Hello"
+b = "good"
+
+print(f"{a}, Python Learner ")
+print(f"I am \"{b}\"")
