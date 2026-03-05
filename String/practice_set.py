50285a7664c2f32eb3386e6b8ad76dfb9627ad69
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-05 23:47:08 +0530

    add some questions

--- String/practice_set.py
+++ String/practice_set.py
@@ -40,8 +40,14 @@ print(text[::-1])
  
 #  @ STRING METHONDS and FUNCTIONS
 
-# 1. Take the string " I love python programming "
+# 4. Take the string " I 
 
+# 1.Using format() , create a sentance
+# love python programming "
+
+#  @ STRING METHONDS and FUNCTIONS
+
+# 4. Take the string " I 
 texe1 = " I love python Programming  "
 
 #   a. Remove the extera spaces from both ends
@@ -67,8 +73,59 @@ else :
 
 # @ STRING FORMATTING AND f-strings
 
-# 1.Using format() , create a sentance
+# 6.Using format() , create a sentance
 
 # My name is johan and I am 25 years old
 # by passing "johan" and 25 as variables
 
+name = "johan"
+age = "25"
+ 
+print("My name is \"{}\"".format(name))
+print("My age is \"{}\"".format(age) )
+
+a = "Guddiya"
+b = "25"
+
+print(f"My name  is {a} and My age is {b}")
+
+
+# @ STRING MANIPULATING CHALANGES
+
+# 7. Given sentences = "Coding in python is fun" , replace "fun" with awosome and print it
+
+Sentence = "Coding in python is fun"
+
+new = Sentence.replace("fun","awsome")
+print(new)
+print(Sentence.replace("fun","awosome"))
+
+# 8. Find the index of the word "python" in sentence
+
+print(Sentence.index("python"))
+
+# 9. Convert the entire sentesnce into uppercase
+
+print(Sentence.upper())
+
+# 10. Write a program that count how many "vowels" in that sentances
+
+sum = 0
+vowels = ['a','e','i','o','u']
+
+for char in Sentence:
+    if(char in vowels):
+        sum += 1
+        
+        
+print(f"There are {sum} vowels in this Sentence")       
+
+
+# 11. Take a user input and check if it is palidrom (same fforward and backwoard)
+
+string2 = "level"
+
+if(string2 == string2 [::-1]):
+    print("Strin2 is palidrom")
+else:
+    print("String2 is not palidrom")    
