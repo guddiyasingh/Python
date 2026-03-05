770c30348a7796b1f6fb90f28403e8483ae58408
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-05 11:20:10 +0530

    add some questions

--- String/practice_set.py
+++ String/practice_set.py
@@ -1,17 +1,59 @@
-# Create a string variable "name" with you full name .Print
+#1 Create a string variable "name" with you full name .Print
 
-# 1. 1st chracter
-# 2. 2nd chracter
-# 3. The length of string
+# a. 1st chracter
+# b. 2nd chracter
+# c. The length of string
 
-name = "Guddiya"
-print(name[0]) # print 1st character
-print(name[-1]) # print 2nd character
-print(len(name))
+# name = "Guddiya"
+# print(name[0]) # print 1st character
+# print(name[-1]) # print 2nd character
+# print(len(name))
 
-# b. concatinate two strings "Hello" and "World" with a space between
+# 2. concatinate two strings "Hello" and "World" with a space between
 
-a = "Hello"
-b = "World"
+# a = "Hello"
+# b = "World"
 
-print(a + " " + b)
+# print(a + " " + b)
+
+
+# 3 STRING SLICING AND iNDEXING 
+
+# a.Given text "Python Programming" do the following
+
+# b. print the first 6 characters
+
+text = "Python Programming"
+
+print(text[0:6])
+
+# c. Print the last 6 characters
+
+print(text[-6:])
+# d. print  every second character of the string
+
+print(text[::2])
+
+# e.Reverse the string text using slicing
+
+print(text[::-1])
+ 
+#  @ STRING METHONDS and FUNCTIONS
+
+# 1. Take the string " I love python programming "
+
+texe1 = " I love python Programming  "
+
+#   a. Remove the extera spaces from both ends
+
+print(texe1.strip())
+
+# Convert into title it to title case
+
+print(texe1.title())
+
+# Count how many times "o" appears
+
+print(texe1.count("o"))
+
+
