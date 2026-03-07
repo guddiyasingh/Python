3f8d76dfc503408e80f129284f1007a7e37853b5
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-07 23:27:52 +0530

    add 'add function in function.py'

--- function_modules/function.py
+++ function_modules/function.py
@@ -2,3 +2,15 @@ def average(a,b,c):
     d = (a+b+c)/2
     return d
 
+o1 = average(2,4,3)
+o2 = average(6,7,8)
+
+print(o1)
+print(o2)
+
+# functions Arguments
+
+def add(a,b):
+    return a+b
+c= add(2,3)
+print(c)
