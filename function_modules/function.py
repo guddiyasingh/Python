5639c7321886fbc75397a3f8b66daa7c7e4e4984
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-09 23:11:46 +0530

    add some code

--- function_modules/function.py
+++ function_modules/function.py
@@ -23,3 +23,35 @@ sum = lambda x,y: x+y
 
 print(square(3))
 print(sum(3,32))
+
+
+
+# fabinisies series
+
+def fib(n):
+
+    if(n == 0 or n == 1):
+        return n
+    
+    return fib(n-2) + fib(n-1)
+
+print(fib(6))
+
+
+def add(a,b):
+    return a+b
+
+# Example Uses
+
+num1 =20
+num2 = 30
+
+print("Sum:", add(num1,num2))
+
+def sub(a,b):
+    return a-b
+
+a =3
+b=4
+
+print("Sub:", sub(a,b))
