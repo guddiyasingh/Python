5d8648604d237365fb549a97667bbdd2fa4096b6
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-03-17 00:30:40 +0530

    add squre question in practice_set.py file

--- function_modules/practice_set.py
+++ function_modules/practice_set.py
@@ -6,3 +6,15 @@ def greet():
     print("Hello python learner")
 
 greet()    
+
+
+
+# Write a function squre(num) that returns the squre of given number .
+#  Test it with different numbers.
+
+def squre(x):
+    return x*x
+
+print(squre(3))
+print(squre(2))
+print(squre(4))
